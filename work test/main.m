ec7875ebcafe9f04bb34b19848041296963060bf
卢佳明 <571836353@qq.com>
2015-11-11 18:47:25 +0800

    Initial Commit

---
+++ work test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  work test
+//
+//  Created by 20141105055ljm on 15/11/11.
+//  Copyright (c) 2015年 20141105055ljm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
